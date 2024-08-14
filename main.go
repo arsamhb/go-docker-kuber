d395a96071f2d03610b8aafde06daf696642e431
Arsam <arsam.hb@gmail.com>
2024-08-14 15:45:46 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("./*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"message": "SUCCESS",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
