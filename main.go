9f69721a688422393942f814d39a84a68a1b84e3
Arsam <arsam.hb@gmail.com>
2024-08-14 18:11:06 +0330

    docker file

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("./*")
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", gin.H{
 			"message": "SUCCESS",
@@ -12,3 +12,5 @@ func main() {
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+// docker run --rm -p 8080:8080 gin-container:latest
