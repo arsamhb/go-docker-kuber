d189d69867709ed18ebd1b461a590e9e2b5c2aa8
Arsam <arsam.hb@gmail.com>
2024-08-17 16:09:19 +0330

    kuber file

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM golang:1.22
+FROM golang:1.22-alpine AS builder
 
 WORKDIR /app
 
 COPY go.mod go.sum ./
-
-RUN go mod download && go mod verify
+RUN go mod download 
 
 COPY . .
 
 RUN go build -o server
+
+FROM alpine:latest
+
+WORKDIR /app
+
+COPY --from=builder /app/server .
 
 CMD [ "./server" ]
--- main.go
+++ main.go
@@ -13,4 +13,8 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-// docker run --rm -p 8080:8080 gin-container:latest
+// WHEN YOU WRITE YOUR DOCKER RUN YOU HAVE TO TAKE A BUILD FROM IT
+// USING docker build -t my-go-server .
+// THEN YOU'LL BE ABLE TO USE THE IMAGE YOU HAVE
+// docker run -d -p 8080:8080 --name go-server my-go-server
+// NOW YOUR CONTAINER IS RUNNING
